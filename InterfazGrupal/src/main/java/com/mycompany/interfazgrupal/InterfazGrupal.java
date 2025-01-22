9114a7388954828f356b100798f1b234b83cdf4c
kar-gl <karenanaluisa40@gmail.com>
2025-01-22 00:18:10 -0500

    interfazKaren

---
+++ InterfazGrupal/src/main/java/com/mycompany/interfazgrupal/InterfazGrupal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfazgrupal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class InterfazGrupal {
+
+    public static void main(String[] args) {
+    InterfazGrupal objInterfaz = new InterfazGrupal();
+    
+    }
+}
